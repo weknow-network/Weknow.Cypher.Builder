59500f3f67189e6504e25fdd3f2eb08209b174e1
Bnaya Eshet <3382669+bnayae@users.noreply.github.com>
2022-11-02 11:22:03 +0200

    test: cypher cards 'With'

--- Tests/Weknow.GraphDbClient.IntegrationTests.Abstract/CypherCards/BaseCypherCardsTests.With.cs
+++ Tests/Weknow.GraphDbClient.IntegrationTests.Abstract/CypherCards/BaseCypherCardsTests.With.cs
@@ -92,4 +92,80 @@ partial class BaseCypherCardsTests
     }
 
     #endregion // MATCH .. WITH user, count(friend) AS friends WHERE friends > 5  
+
+    #region MATCH .. WITH user, count(friend) AS friends WHERE friends > 5 ORDER BY friends DESC   
+
+    [Fact]
+    public virtual async Task With_OrderBy_Test()
+    {
+        CypherConfig.Scope.Value = CONFIGURATION;
+        var users = Parameters.Create();
+        var friends = Variables.Create();
+        var userName = Parameters.Create<string>();
+        var (user, friend, map) = Variables.CreateMulti<PersonEntity, PersonEntity, PersonEntity>();
+
+        #region Prepare
+
+        CypherCommand cypherOfUsers = _(() =>
+                                Unwind(users, map,
+                                     Merge(N(user, Person, new { key = (~map)._.key /* result in map.key*/ }))
+                                       .Set(user, map)));
+
+
+        CypherParameters prms = cypherOfUsers.Parameters;
+        var usersPrm = Enumerable.Range(0, 10)
+                                .Select(UserFactory)
+                                .ToArray();
+        prms.AddRange(nameof(users), usersPrm);
+        await _graphDB.RunAsync(cypherOfUsers, prms);
+        foreach (var u in usersPrm)
+        {
+            var id = Parameters.Create();
+            CypherCommand cypherOfFriends = _(() =>
+                                    Match(N(user, Person))
+                                    .Where(user._.key == id)
+                                    .With(user)
+                                    .Unwind(friends.AsParameter, map,
+                                         Merge(N(friend, Friend, new { key = (~map)._.key }))
+                                            .Set(friend, map)
+                                         .Merge(N(user) - R[Knows] > N(friend))));
+
+            prms = cypherOfFriends.Parameters;
+            var friendsPrm = Enumerable.Range(0, (u.key ?? 0))
+                                    .Select(m => FriendFactory(u, m))
+                                    .ToArray();
+            prms.AddRange(nameof(friends), friendsPrm);
+            prms.AddValue(nameof(id), u.key ?? 0);
+            await _graphDB.RunAsync(cypherOfFriends, prms);
+        }
+
+        #endregion // Prepare
+
+        CypherCommand query = _(() =>
+                                Match(N(user, Person) - R[Knows] > N(friend, Friend))
+                                .With(user, friend.Count().As(friends))
+                                .Where(friends > 5)
+                                .Return(user)
+                                .OrderByDesc(friends));
+        _outputHelper.WriteLine($"CYPHER: {query}");
+        IGraphDBResponse response = await _graphDB.RunAsync(query, query.Parameters);
+        var results = await response.GetRangeAsync<PersonEntity>(nameof(user)).ToArrayAsync();
+
+        #region Validation
+
+        Assert.True(results.Length == 4);
+        for (int i = 0; i < 4; i++)
+        {
+            var item = UserFactory(9 - i );
+            var res = results[i];
+            Assert.Equal(item, res);
+        }
+
+        #endregion // Validation
+
+        PersonEntity UserFactory(int i) => new PersonEntity($"User {i}", i + 30) { key = i };
+        PersonEntity FriendFactory(PersonEntity user, int i) => new PersonEntity($"Friend {i} of {user.name}", i + 30) { key = i + 10 + (user.key * 100) };
+    }
+
+    #endregion // MATCH .. WITH user, count(friend) AS friends WHERE friends > 5 ORDER BY friends DESC
 }
